2659909212ac2688e15ce8e5000a72d136d3fcf5
JehandadK <s.jehandad.kamal@gmail.com>
2015-04-02 08:36:01 +0500

    minor fixes

--- random_forest.py
+++ random_forest.py
@@ -1,11 +1,9 @@
 # Random Forest implementation to match and compare results in R
-# Is MultiThread or MultiCore? No
+# Is MultiThread or MultiCore? Yes
 # Is GPGPU? No
 from sklearn.metrics import classification_report
-from sklearn.ensemble import RandomForestClassifier
 from sklearn import ensemble, feature_extraction, preprocessing
 import pandas as pd
-import numpy as np
 import time
 import yaml
 import sys
@@ -13,7 +11,7 @@ import psutil
 
 startTime = time.time()
 
-#load data
+# load data
 train = pd.read_csv('data/train.csv')
 test = pd.read_csv('data/test.csv')
 sample = pd.read_csv('submissions/sampleSubmission.csv')
@@ -30,6 +28,7 @@ test = test.drop('id', axis=1)
 # ideally dont use defaults, always send through args
 class Foo(object):
     pass
+
 
 config = Foo()
 config.estimators = 100
@@ -62,17 +61,17 @@ config.score = classification_report(train_pred, labels)
 # create submission file
 timestr = time.strftime("%m%d-%H%M%S")
 preds = pd.DataFrame(preds, index=sample.id.values, columns=sample.columns[1:])
-preds.to_csv('./submissions/random-forest-'+timestr+'.csv', index_label='id')
+preds.to_csv('./submissions/random-forest-' + timestr + '.csv', index_label='id')
 
 # Write config data
 data = dict(
-	start_time = time.strftime("%b %d %Y %H:%M:%S", time.gmtime(startTime)),
-    end_time = time.strftime("%b %d %Y %H:%M:%S"),
-    elapsedSeconds = time.time() - startTime,
-    config = config.__dict__
+    start_time=time.strftime("%b %d %Y %H:%M:%S", time.gmtime(startTime)),
+    end_time=time.strftime("%b %d %Y %H:%M:%S"),
+    elapsedSeconds=time.time() - startTime,
+    config=config.__dict__
 )
-with open('./submissions/random-forest-'+timestr+'.yaml', 'w') as outfile:
-    outfile.write( yaml.dump(data, default_flow_style=False) )
+with open('./submissions/random-forest-' + timestr + '.yaml', 'w') as outfile:
+    outfile.write(yaml.dump(data, default_flow_style=False))
 
 print yaml.dump(data, allow_unicode=True, default_flow_style=False)
 
